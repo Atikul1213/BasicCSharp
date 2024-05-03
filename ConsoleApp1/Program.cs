4691591f54ae8b4774e75ae5856c109dc44e0bb2
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-03 14:49:30 +0600

    Enum

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,26 +12,26 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            DateTime dateTime = new DateTime(2015, 1, 1);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
-
-            var hour = now.Hour;
-            var minute = now.Minute;
-
-            var tomorrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
-
-            Console.WriteLine(now.ToLongDateString());
-            Console.WriteLine(now.ToShortDateString());
-            Console.WriteLine(now.ToLongTimeString());
-            Console.WriteLine(now.ToShortTimeString());
-            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+            var season = Season.Antumn;
+            switch (season)
+            {
+                case Season.Antumn:
+                    Console.WriteLine("It is antumn");
+                    break;
+                case Season.Spring:
+                    Console.WriteLine("it is Spring");
+                    break;
+                default:
+                    Console.WriteLine("It is other");
+                    break;
+            }
 
 
 
 
             Console.ReadLine();
         }
+
+        
     }
 }
