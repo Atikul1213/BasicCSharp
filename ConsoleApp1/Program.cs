4e922a328c251da9c8c3f0b747b89438fa59341d
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-03 14:33:06 +0600

    define class and use

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,20 +11,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            int n = 10;
-            double d = 5.25;
-            bool ck = true;
-            Console.WriteLine(Convert.ToString(n));
-            Console.WriteLine(Convert.ToDouble(n));
-            Console.WriteLine(Convert.ToInt32(n));
-            Console.WriteLine(Convert.ToString(ck));
 
-            
-             float f = 1.0f;
-            int i = (int)f;
+            Person person = new Person();
 
-            string s = "1";
-            int j = Convert.ToInt32(s);
+            person.firstName = "Atikul";
+            person.lastName = "Islam";
+
+            person.Introduce();
+
             Console.ReadLine();
         }
     }
