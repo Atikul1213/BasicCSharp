c53727db6640d04de6c9a63c71d909d4945b65f6
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-03 14:46:06 +0600

    DateTime

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,21 +12,21 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            var number = new List<int>();
-            var numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
-            numbers.Add(10);
-            numbers.AddRange(new int[3] { 11, 12, 14 });
-            var idx = numbers.IndexOf(14);
-            idx = numbers.LastIndexOf(14);
-            var count = numbers.Count();
-            numbers.Remove(14);
-            numbers.RemoveAt(0);
-            numbers.Clear();
+            DateTime dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
 
-            foreach (var x in numbers)
-            {
-                Console.WriteLine("x: " + x);
-            }
+            var hour = now.Hour;
+            var minute = now.Minute;
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
 
 
 
