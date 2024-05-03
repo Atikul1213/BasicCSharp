3acb77f80cc6e2243e6d30aa3634dd81bc7d1b11
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-03 14:11:29 +0600

    Data Types

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,32 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            byte Num = 3;
+            int count = 10;
+            float totalPrice = 10.34f;
+            char character = 'A';
+            string firstName = "Atikul";
+            bool isWorking = true;
+
+
             Console.WriteLine("Hello World");
+            Console.WriteLine("Num: " + Num);
+            Console.WriteLine("count: " + count);
+            Console.WriteLine("totalPrice: " + totalPrice);
+            Console.WriteLine("Character: " + character);
+            Console.WriteLine("firstName: " + firstName);
+            Console.WriteLine("isWorking: " + isWorking);
+
+
+            var lastName = "Islam";     // Take any type of dataType
+            Console.WriteLine("LastName: " + lastName);
+
+            Console.WriteLine("{0} {1}", byte.MaxValue, byte.MinValue);
+
+
+            const float pi = 3.1416f;
+            Console.WriteLine("Constant: " + pi);
+
             Console.ReadLine();
         }
     }
