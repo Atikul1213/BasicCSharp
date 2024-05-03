eeb27afb1d01257a29ba2bb930ba986f5fd3ea22
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-03 14:38:29 +0600

    Array operation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,10 +13,28 @@ namespace ConsoleApp1
         {
 
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(10, 20);
+            int[] number = new int[3] { 10, 20, 30 };
+            number[0] = 10;
+            number[1] = 20;
+            number[2] = 30;
 
-            Console.WriteLine("Result: " + result);
+            foreach(var x in number)
+            {
+                Console.WriteLine("x : "+x);
+            }
+
+            var numbers = new int[] { 3, 7, 9, 10, 11, 12, 13 };
+
+            var len = numbers.Length;
+            var index = Array.IndexOf(numbers, 9);
+            Array.Clear(numbers, 0, 2);
+
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
 
 
             Console.ReadLine();
