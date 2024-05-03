5ab8bade2033a67deb0ad34d066c4410a8449fd7
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-03 14:45:11 +0600

    list

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,24 +11,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int n = 5;
-            int i = 0;
-            while (i < n)
+
+            var number = new List<int>();
+            var numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            numbers.Add(10);
+            numbers.AddRange(new int[3] { 11, 12, 14 });
+            var idx = numbers.IndexOf(14);
+            idx = numbers.LastIndexOf(14);
+            var count = numbers.Count();
+            numbers.Remove(14);
+            numbers.RemoveAt(0);
+            numbers.Clear();
+
+            foreach (var x in numbers)
             {
-                Console.WriteLine(i);
-                i = i + 1;
+                Console.WriteLine("x: " + x);
             }
-
-            for (i = 0; i <= n; i++)
-            {
-                Console.WriteLine(i);
-            }
-
-
-            int[] number = new int[3] { 10, 20, 30 };
-
-            foreach (var num in number)
-                Console.WriteLine(num);
 
 
 
