322bdb16fc6d7e9b863ecd60c6db1b9b16033a84
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-04 17:36:03 +0600

    abstract class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,17 +15,9 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
 
-            Car car = new Car(100, "blue");
-            car.Print();
-
-            Bmw bmw = new Bmw(200, "Red", "R34");
-            bmw.Print();
-            bmw.Repair();
-
-
-            Audi audi = new Audi(300, "Yellow", "K322");
-            audi.Print();
-            audi.Repair();
+            Pig myPig = new Pig();
+            myPig.animalSound();
+            myPig.sleep();
 
             Console.ReadLine();
         }
@@ -33,76 +25,23 @@ namespace ConsoleApp1
     }
 
 
-    public class Car
+     public abstract class Animal
     {
-        public int hp { get; set; }
-        public string color { get; set; }
+        public abstract void animalSound();
 
-        public Car(int _hp, string _color)
+        public void sleep()
         {
-            this.hp = _hp;
-            this.color = _color;
-        }
-
-        public virtual void Print()
-        {
-            Console.WriteLine("Hp: "+hp+ " color: "+color);
-        }
-
-        public virtual void Repair()
-        {
-            Console.WriteLine("Car was repaired");
-        }
-
-    }
-
-
-
-    class Bmw : Car
-    {
-        private string brand = "bmw";
-        public string Model { get; set; }
-
-        public Bmw(int _hp,string _color, string _Model):base(_hp,_color)
-        {
-            this.Model = _Model;
-        }
-
-        public override void Print()
-        {
-            Console.WriteLine("Brand: "+brand+" Hp: "+hp+" color: "+color+" Model: "+Model);
-
-        }
-
-        public override void Repair()
-        {
-            Console.WriteLine("Bmw {0} car was repaired",brand);
+            Console.WriteLine("Zzz");
         }
     }
 
-
-    class Audi : Car
+     public class Pig : Animal
     {
-        private string brand = "Audi";
-        public string Model { get; set; }
-
-        public Audi(int _hp,string _color,string _Model) : base(_hp,_color)
+        public override void animalSound()
         {
-            this.Model = _Model;
-        }
-
-        public override void Print()
-        {
-            Console.WriteLine("Brand: "+brand+" Model: "+Model+" hp: "+hp+" color:"+color);
-
-        }
-
-        public override void Repair()
-        {
-            Console.WriteLine("Bmw{0} car was repaired",brand);
+            Console.WriteLine("The pig say: wee wee");
         }
     }
-
 
 
 
