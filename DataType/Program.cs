5e464ee86e58c3f56201154058a48bc7b8a3ec12
Atikul1213 <atikul.islam@brainstation-23.com>
2024-10-13 22:47:00 +0600

    Learn DataType

---
+++ DataType/Program.cs
@@ -0,0 +1,9 @@
+﻿using DataType;
+
+string str = "Atikul";
+
+Console.WriteLine("Total size: " + str.Length);
+Console.WriteLine(sizeof(Char));
+
+Test test = new Test();
+Console.WriteLine("Roll: " + test.Roll + " Name: " + test.Name + " isVoter: " + test.IsVoter);
---
+++ DataType/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace DataType
+{
+    public class Test
+    {
+        public int Roll;
+        public string Name;
+        public bool IsVoter;
+        public Test()
+        {
+
+        }
+    }
+}
