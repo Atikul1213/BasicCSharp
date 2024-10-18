9ea2014c8130f3020e3ded54946660691d0f0ab9
Atikul1213 <atikul.islam@brainstation-23.com>
2024-10-18 23:16:56 +0600

    Random number

---
+++ RandomNumberDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace RandomNumberDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Random: " + random.Next());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Randdom: " + random.Next(5, 11));
+                Console.WriteLine("Random double: " + random.NextDouble());
+                SimpleMethod(random);
+
+            }
+
+            List<PersonModel> people = new List<PersonModel>()
+            {
+                new PersonModel{FirstName = "John"},
+                new PersonModel{FirstName = "Steve"},
+                new PersonModel{FirstName = "Bill"}
+            };
+
+            var sortedPeople = people.OrderBy(x => random.Next());
+            foreach (var p in sortedPeople)
+            {
+                Console.WriteLine(p.FirstName);
+            }
+        }
+
+
+        private static void SimpleMethod(Random random)
+        {
+            Console.WriteLine(random.Next());
+        }
+    }
+
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+    }
+}
+
+
+
+
