a9cd458d6f48ae44c84fb1e348824ea9c3a27de7
Atikul1213 <atikul.islam@brainstation-23.com>
2024-11-26 09:41:27 +0600

    Generic and Non Generic collection

---
+++ GenericCollection/Program.cs
@@ -0,0 +1,79 @@
+﻿
+public class GenericCollection
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("atikul");
+
+        //List
+        List<int> lst = new List<int>();
+        lst.Add(1);
+        lst.Add(2);
+        lst.Add(3);
+        lst.Add(4);
+        lst.Add(5);
+
+        foreach (var item in lst)
+        {
+            Console.WriteLine("list value: " + item);
+        }
+
+
+
+        //Dictionary
+        Dictionary<int, string> dict = new Dictionary<int, string>();
+        dict.Add(1, "Atikul");
+        dict.Add(3, "Ismail");
+        dict.Add(2, "Ripa");
+        dict.Add(4, "Atik");
+
+        foreach (KeyValuePair<int, string> kvp in dict)
+        {
+            Console.WriteLine("dictionary key: " + kvp.Key + "  value: " + kvp.Value);
+        }
+
+
+        // Sorted List
+
+        SortedList<string, string> slist = new SortedList<string, string>();
+
+        slist.Add("American", "Burger");
+        slist.Add("LIme", "soda");
+        slist.Add("French", "Fries");
+        slist.Add("Onion", "Rings");
+
+        foreach (KeyValuePair<string, string> kvp in slist)
+        {
+            Console.WriteLine("SortedList key: " + kvp.Key + " value: " + kvp.Value);
+        }
+
+
+
+        //Stack 
+        Stack<string> st = new Stack<string>();
+        st.Push("Rare");
+        st.Push("Medium Rare");
+        st.Push("Medium");
+        st.Push("Well done");
+
+        foreach (var item in st)
+        {
+            Console.WriteLine("Stack value: " + item);
+        }
+
+
+
+        //Queue
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue("Mark");
+        queue.Enqueue("Bill");
+        queue.Enqueue("Xavier");
+        queue.Enqueue("Micheal");
+
+        foreach (var item in queue)
+        {
+            Console.WriteLine("queue value: " + item);
+        }
+    }
+
+}
---
+++ NonGenericCollection/Program.cs
@@ -0,0 +1,80 @@
+﻿
+using System.Collections;
+
+public class GenericCollection
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("atikul");
+
+        // Array List
+
+        ArrayList alist = new ArrayList();
+        string str = "I am atikul";
+        int x = 11;
+        DateTime dt = DateTime.Parse("3-dec-1999");
+        alist.Add(str);
+        alist.Add(x);
+        alist.Add(dt);
+
+        foreach (var item in alist)
+        {
+            Console.WriteLine("Arraylist value: " + item);
+        }
+
+
+
+        // HashTable 
+        Hashtable htable = new Hashtable();
+        htable.Add(2, "Soda");
+        htable.Add(1, "Burger");
+        htable.Add(4, "Fries");
+        htable.Add(3, "Onion Rings");
+
+        foreach (DictionaryEntry ht in htable)
+        {
+            Console.WriteLine("Hashtable key: " + ht.Key + " value: " + ht.Value);
+        }
+
+
+        //Sorted list
+        SortedList slist = new SortedList();
+        slist.Add("America", "Burgerr");
+        slist.Add("Lime", "Soda");
+        slist.Add("French", "Fries");
+        slist.Add("Onion", "Rings");
+
+        foreach (DictionaryEntry sl in slist)
+        {
+            Console.WriteLine("SortedList key: " + sl.Key + "  value: " + sl.Value);
+        }
+
+
+        //Stack
+
+        Stack st = new Stack();
+        st.Push("Rare");
+        st.Push("Medium Rare");
+        st.Push("Medium");
+        st.Push("Well done");
+
+        foreach (var item in st)
+        {
+            Console.WriteLine("Stack value: " + item);
+        }
+
+
+        // Queue 
+        Queue queue = new Queue();
+        queue.Enqueue("Mark");
+        queue.Enqueue("Bill");
+        queue.Enqueue("Xavier");
+        queue.Enqueue("Michael");
+
+        foreach (var item in queue)
+        {
+            Console.WriteLine("queue value: " + item);
+        }
+    }
+
+}
