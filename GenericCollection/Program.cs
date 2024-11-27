4c89f53623f3f227ad24410dcaaf729a09532d3b
Atikul1213 <atikul.islam@brainstation-23.com>
2024-11-27 22:25:01 +0600

    Generic collection

--- GenericCollection/Program.cs
+++ GenericCollection/Program.cs
@@ -33,6 +33,21 @@ public class GenericCollection
         }
 
 
+        // SortedDictionary
+        SortedDictionary<int, string> sdict = new SortedDictionary<int, string>();
+        sdict.Add(1, "Atikul");
+        sdict.Add(3, "Ismail");
+        sdict.Add(2, "Ripa");
+        sdict.Add(4, "Atik");
+        foreach (KeyValuePair<int, string> kvp in sdict)
+        {
+            Console.WriteLine("sort Dictionary key: " + kvp.Key + " value: " + kvp.Value);
+        }
+
+
+
+
+
         // Sorted List
 
         SortedList<string, string> slist = new SortedList<string, string>();
@@ -55,13 +70,16 @@ public class GenericCollection
         st.Push("Medium Rare");
         st.Push("Medium");
         st.Push("Well done");
+        st.Pop();
+        st.Peek();
+
 
         foreach (var item in st)
         {
             Console.WriteLine("Stack value: " + item);
         }
 
-
+        st.Clear();
 
         //Queue
         Queue<string> queue = new Queue<string>();
@@ -69,11 +87,58 @@ public class GenericCollection
         queue.Enqueue("Bill");
         queue.Enqueue("Xavier");
         queue.Enqueue("Micheal");
+        queue.Dequeue();
+        queue.Peek();
 
         foreach (var item in queue)
         {
             Console.WriteLine("queue value: " + item);
         }
+
+        queue.Clear();
+
+        // HashSet
+
+        HashSet<int> hset = new HashSet<int>();
+        hset.Add(11);
+        hset.Add(22);
+        hset.Add(55);
+        hset.Add(65);
+        hset.Add(55);
+
+        foreach (var item in hset)
+        {
+            Console.WriteLine("value: " + item);
+        }
+
+
+
+        // Sorted set
+        SortedSet<int> sset = new SortedSet<int>();
+
+        sset.Add(11);
+        sset.Add(22);
+        sset.Add(55);
+        sset.Add(88);
+        sset.Add(22);
+        sset.Add(77);
+
+
+        foreach (var item in sset)
+        {
+            Console.WriteLine("value: " + item);
+        }
+
+
+
+        //Linked list
+
+        LinkedList<string> linkList = new LinkedList<string>();
+        linkList.AddLast("one");
+        linkList.AddFirst("two");
+        linkList.AddLast("three");
+        linkList.AddFirst("four");
+
     }
 
 }
--- NonGenericCollection/Program.cs
+++ NonGenericCollection/Program.cs
@@ -45,6 +45,8 @@ public class GenericCollection
         alist.Clear();
 
 
+
+
         // HashTable 
         Hashtable htable = new Hashtable();
         htable.Add(2, "Soda");
@@ -52,13 +54,27 @@ public class GenericCollection
         htable.Add(4, "Fries");
         htable.Add(3, "Onion Rings");
 
+        if (htable.Contains(3))
+        {
+            htable.Remove(3);
+        }
+        htable.ContainsKey(3);
+        htable.ContainsValue("Soda");
+
         foreach (DictionaryEntry ht in htable)
         {
             Console.WriteLine("Hashtable key: " + ht.Key + " value: " + ht.Value);
         }
 
+        Hashtable cloneHashtable = (Hashtable)htable.Clone();
+
+        htable.Clear();
+
+
+
 
         //Sorted list
+        // key not duplicate and  key not support different data type 
         SortedList slist = new SortedList();
         slist.Add("America", "Burgerr");
         slist.Add("Lime", "Soda");
@@ -71,6 +87,31 @@ public class GenericCollection
         }
 
 
+        for (int i = 0; i < slist.Count; i++)
+        {
+            Console.WriteLine("Key: " + slist.GetKey(i) + " value: " + slist.GetByIndex(i));
+        }
+        slist.Remove("Onion");
+        slist.RemoveAt(1);
+        if (slist.Contains("Onion"))
+        {
+
+        }
+        if (slist.ContainsKey("Onion"))
+        {
+
+        }
+        if (slist.ContainsValue("Onion"))
+        {
+
+        }
+        var cloneSlist = (SortedList)slist.Clone();
+        slist.Clear();
+
+
+
+
+
         //Stack
 
         Stack st = new Stack();
@@ -78,11 +119,23 @@ public class GenericCollection
         st.Push("Medium Rare");
         st.Push("Medium");
         st.Push("Well done");
+        st.Push(12);
+        st.Push(67.4);
+        st.Push(true);
 
         foreach (var item in st)
         {
             Console.WriteLine("Stack value: " + item);
         }
+
+        if (st.Contains(12))
+        {
+
+        }
+        var peek = st.Peek();
+        var cloneStack = (Stack)st.Clone();
+        st.Clear();
+
 
 
         // Queue 
@@ -91,11 +144,19 @@ public class GenericCollection
         queue.Enqueue("Bill");
         queue.Enqueue("Xavier");
         queue.Enqueue("Michael");
+        queue.Enqueue(67.5);
+        queue.Enqueue(true);
+
+        queue.Dequeue();
+        var peekValue = queue.Peek();
 
         foreach (var item in queue)
         {
             Console.WriteLine("queue value: " + item);
         }
+
+        var cloneQueue = (Queue)queue.Clone();
+        queue.Clear();
     }
 
 }
