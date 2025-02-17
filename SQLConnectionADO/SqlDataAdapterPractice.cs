2fce2eea3204da8b338cc89292f24e11917bfb1b
Atikul1213 <atikul.islam@brainstation-23.com>
2025-02-17 09:48:08 +0600

    SqlData Adapter

---
+++ SQLConnectionADO/SqlDataAdapterPractice.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SQLConnectionADO
+{
+    public class SqlDataAdapterPractice
+    {
+
+        public void DataAdapter()
+        {
+            string cs = "Data Source = BS-01344; Initial Catalog = AspnetB11; Integrated Security = true";
+            SqlConnection con = null;
+
+            using (con = new SqlConnection(cs))
+            {
+                string query = "select * from Couse1";
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    Console.WriteLine("Id: " + dr[0] + "  Name: " + dr[1] + " Fee: " + dr[2]);
+                }
+            }
+        }
+
+    }
+}
