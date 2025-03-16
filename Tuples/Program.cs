6a77a32d5554cee3ece636823de032a76301f631
Atikul1213 <atikul.islam@brainstation-23.com>
2025-03-16 10:51:59 +0600

    tuple practice

---
+++ Tuples/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tuples;
+
+Console.WriteLine("Hello, World!");
+
+var tupleExample = new TupleExample();
+
+var values = new List<double> { 1.0, 2.0, 3.0, 4.0, 5.0 };
+
+var result = tupleExample.Calculate(values);
+
+Console.WriteLine($"There are {result.Item1} values and their sum is {result.Item2}");
+
+
+// New way of returning multiple values
+
+var result1 = tupleExample.Calculate1(values);
+Console.WriteLine($"There are {result.Item1} values and their sum is {result.Item2}");
+
+
+// Get Employee Details
+
+(string name, double salary, string company, string designation) = tupleExample.GetEmployeeDetails(1);
+
+Console.WriteLine($"Name: {name}  salary: {salary} Company: {company} designation: {designation}");
---
+++ Tuples/TupleExample.cs
@@ -0,0 +1,46 @@
+﻿namespace Tuples
+{
+    public class TupleExample
+    {
+        // Old way of returning multiple values
+        public Tuple<int, double> Calculate(IEnumerable<double> values)
+        {
+            int count = 0;
+            double sum = 0.0;
+            foreach (var value in values)
+            {
+                count++;
+                sum += value;
+            }
+
+            Tuple<int, double> tuple = Tuple.Create(count, sum);
+
+            return tuple;
+        }
+
+        // New way of returning multiple values
+        public (int, double) Calculate1(IEnumerable<double> values)
+        {
+            int count = 0;
+            double sum = 0.0;
+            foreach (var value in values)
+            {
+                count++;
+                sum += value;
+            }
+
+            return (count, sum);
+        }
+
+
+        public (string, double, string, string) GetEmployeeDetails(long EmployeeId)
+        {
+            string name = "Atikul Islam";
+            double salary = 100000.00;
+            string company = "Brain Station 23";
+            string designation = "Software Engineer";
+
+            return (name, salary, company, designation);
+        }
+    }
+}
