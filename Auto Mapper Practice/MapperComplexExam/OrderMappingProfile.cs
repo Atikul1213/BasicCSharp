7176e66b59da4604ec61d5ec14b1b25fb3837273
Atikul1213 <atikul.islam@brainstation-23.com>
2025-03-22 22:32:57 +0600

    add mapper

---
+++ Auto Mapper Practice/MapperComplexExam/OrderMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using Auto_Mapper_Practice.MapperComplexExam.DTO;
+using AutoMapper;
+
+namespace Auto_Mapper_Practice.MapperComplexExam
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            //  CreateMap<TSource, TDestination>() 
+            //  Map<TDestination>(object source)
+
+            CreateMap<Order, OrderDTO>()
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.OrderDate, opt => opt.MapFrom(src => src.OrderDate.ToString("yyyy-MM-dd")))
+                .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => $"{src.Customer.FirstName} {src.Customer.LastName}"))
+                .ForMember(dest => dest.CustomerEmail, opt => opt.MapFrom(src => src.Customer.Email))
+                .ForMember(dest => dest.CustomerPhoneNumber, opt => opt.MapFrom(src => src.Customer.PhoneNumber))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Customer.Address))
+                .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.OrderItems));
+
+
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.ProductPrice, opt => opt.MapFrom(src => src.Product.Price));
+        }
+    }
+}
