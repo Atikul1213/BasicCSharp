a001eebaaeff8dee6bd0272010c89b859fbb45d5
Atikul1213 <atikul.islam@brainstation-23.com>
2025-03-22 20:03:06 +0600

    product Mapping example

---
+++ Auto Mapper Practice/ProductMapper/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Auto_Mapper_Practice.ProductMapper
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string? SKU { get; set; }
+        // Renamed properties to simulate different naming conventions
+        public string ProductName { get; set; }
+        public string? ShortDescription { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Auto Mapper Practice/ProductMapper/ProductMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace Auto_Mapper_Practice.ProductMapper
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<Product, ProductDTO>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.ShortDescription, opt => opt.MapFrom(src => src.Description));
+
+            CreateMap<ProductCreateDTO, Product>();
+        }
+
+        //  CreateMap<TSource, TDestination>() 
+        //  Map<TDestination>(object source)
+        //  var productDTO = _mapper.Map<ProductDTO>(product);
+    }
+}
