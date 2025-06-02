1db29e3cc4726ccd266872e514462678e4e8a010
Atikul1213 <atikul.islam@brainstation-23.com>
2025-06-02 23:23:48 +0600

    aws send message

---
+++ AWSSendMessage/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+Console.WriteLine("Hello, World!");
+
+RegionEndpoint ServiceRegion = RegionEndpoint.USEast1;
+IAmazonSQS client = new AmazonSQSClient(ServiceRegion);
+
+var url = "https://sqs.us-east-1.amazonaws.com/424557340333/aspnet-b11-queue";
+var body = "Hello I am Atikul From Brain Station";
+
+await SendMessage(client, url, body, null);
+
+static async Task<SendMessageResponse> SendMessage(
+          IAmazonSQS client,
+          string queueUrl,
+          string messageBody,
+          Dictionary<string, MessageAttributeValue> messageAttributes)
+{
+    var sendMessageRequest = new SendMessageRequest
+    {
+        DelaySeconds = 10,
+        MessageAttributes = messageAttributes,
+        MessageBody = messageBody,
+        QueueUrl = queueUrl,
+    };
+
+    var response = await client.SendMessageAsync(sendMessageRequest);
+    Console.WriteLine($"Sent a message with id : {response.MessageId}");
+
+    return response;
+}
